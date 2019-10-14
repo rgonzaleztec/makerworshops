1d4829f6d75caa67d8ddfa164fdbfe31c54e18be
Rogelio <rogelio.rgonzalez@gmail.com>
2019-10-13 20:36:20 -0600

    Plantilla base de ejemplo
    Base de CPG

---
+++ codigo/BasicoArduino.ino
@@ -0,0 +1,40 @@
+#include <Adafruit_CircuitPlayground.h>
+// Libreria que se requiere para trabajar con la placa
+
+
+void setup() {
+  // put your setup code here, to run once:
+ // Limpiamos los colores que tenga el circuito anteriormente programados
+  CircuitPlayground.begin();
+ 
+  CircuitPlayground.clearPixels();
+}
+
+void loop() {
+  // Codigo que se repite eternamente
+
+  encenderLed(1, 255,0,0,255);
+  delay(300);
+  encenderLed(4, 255,0,0,255);
+  delay(300);
+  encenderLed(7, 255,0,0,255);
+  delay(500);
+  CircuitPlayground.clearPixels();
+  delay(1000);
+
+}
+
+
+
+void encenderLed(int pixelNum, int rojo, int verde, int azul, int intensidad)
+{
+  // Funcion para configurar un color a un pixel
+  // Los valores permitidos para los colores van de 0 a 255
+  // La intensidad va de 0 a 255, siendo 0 apagado y 255 muy intenso
+  // pixelNum van del 0 al 9
+  
+  
+  CircuitPlayground.setPixelColor(pixelNum, rojo, verde, azul);
+  CircuitPlayground.setBrightness(intensidad);
+  
+}
